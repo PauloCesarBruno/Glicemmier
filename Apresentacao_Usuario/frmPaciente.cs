6958d5d7cf656b82df11d2269db1f0a4ac2bdf57
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2021-01-24 11:42:19 -0300

    Glicemmier 2.0

--- Apresentacao_Usuario/frmPaciente.cs
+++ Apresentacao_Usuario/frmPaciente.cs
@@ -250,8 +250,7 @@ namespace Apresentacao_Usuario
             }
             // CapCelulaParametros();
             lblcabecalho.Text = "Parâmetros Clicêmicos de Nicolle Bueno Cordovil Bruno do Mês: " + DateTime.Now.Month.ToString() + " de " + DateTime.Now.Year.ToString();
-
-            //this.reportViewer1.RefreshReport();
+                                               
         }
 
         private void btnAtualizar_Click(object sender, EventArgs e)
